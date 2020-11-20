88ce065cd527159fa6af65e973897a9c3a2cca7a
alexgriffen <alex.griffen@saucelabs.com>
2020-11-20 12:18:03 -0800

    add tunnelId and update pom

--- src/test/java/com/saucelabs/example/TestSetup.java
+++ src/test/java/com/saucelabs/example/TestSetup.java
@@ -1,30 +1,21 @@
 package com.saucelabs.example;
 
-//import com.saucelabs.example.util.ResultReporter;
-
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
-
 import org.openqa.selenium.*;
-import org.openqa.selenium.html5.Location;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by grago on 28/02/2017.
@@ -32,13 +23,38 @@ import java.net.URL;
 public class TestSetup {
 
 //    private ResultReporter reporter;
-    private ThreadLocal<IOSDriver> driver = new ThreadLocal<IOSDriver>();
+//    private ThreadLocal<IOSDriver> driver = new ThreadLocal<IOSDriver>();
 
     public String username = System.getenv("SAUCE_USERNAME");
 
     public String accesskey = System.getenv("SAUCE_ACCESS_KEY");
 
-    public String build = System.getenv("BUILD_TAG");
+    public String buildTag = System.getenv("BUILD_TAG");
+
+    /**
+     * ThreadLocal variable which contains the  {@link IOSDriver} instance which is used to perform browser interactions with.
+     */
+    private ThreadLocal<IOSDriver> driver = new ThreadLocal<IOSDriver>();
+
+    /**
+     * ThreadLocal variable which contains the Sauce Job Id.
+     */
+    private ThreadLocal<String> sessionId = new ThreadLocal<String>();
+
+    /**
+     * @return the {@link IOSDriver} for the current thread
+     */
+    public IOSDriver getIOSDriver() {
+        return driver.get();
+    }
+
+    /**
+     *
+     * @return the Sauce Job id for the current thread
+     */
+    public String getSessionId() {
+        return sessionId.get();
+    }
 
   /**
    * DataProvider that explicitly sets the browser combinations to be used.
@@ -67,7 +83,7 @@ public class TestSetup {
       };
   }
 
-  private IOSDriver createDriver(String platformName, String platformVersion, String deviceName, String methodName) throws MalformedURLException {
+  protected IOSDriver createDriver(String platformName, String platformVersion, String deviceName, String methodName) throws MalformedURLException {
 
       DesiredCapabilities capabilities = new DesiredCapabilities();
 //       capabilities.setCapability("testobject_api_key", "REDACTED");
@@ -77,7 +93,8 @@ public class TestSetup {
       capabilities.setCapability("platformVersion", platformVersion);
       capabilities.setCapability("platformName", platformName);
       capabilities.setCapability("name",  methodName);
-      capabilities.setCapability("tunnelIdentifier", "TUNNEL_ID");
+      capabilities.setCapability("build",  buildTag);
+      capabilities.setCapability("tunnelIdentifier", "allTheRDC");
       capabilities.setCapability("appiumVersion", "1.17.1");
 //      capabilities.setCapability("browserName", "Safari");
 //      capabilities.setCapability("app", "storage:dc565179-cb52-4ca3-a25d-484b8136a542"); //UP RDC
@@ -85,13 +102,21 @@ public class TestSetup {
       capabilities.setCapability("app", "https://github.com/alexgriffen/RDC-Java-TestNG-Appium-iOS-Dynamic/raw/master/CalculatorAlex.ipa"); // remote hosted for CI
 
 
+
       driver.set(new IOSDriver<WebElement>(
 //               new URL("https://us1.appium.testobject.com/wd/hub"), //Legacy RDC TestObject
 //              new URL("http://localhost:4455/wd/hub"),
               new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub"), //New Unified Platform Awesomeness
               capabilities));
+      String id = getIOSDriver().getSessionId().toString();
+      sessionId.set(id);
+      System.out.println(id + " is the ID for this session");
       return driver.get();
+
   }
+
+
+
 
     /* A simple addition, it expects the correct result to appear in the result field. */
     @Test(dataProvider = "devices")
@@ -99,7 +124,10 @@ public class TestSetup {
 
     	IOSDriver driver = createDriver(platformName, platformVersion, deviceName, method.getName());
 
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 //        /* Get the elements. Bypass this for everything but resultField */
+        MobileElement buttonTwo = (MobileElement)(driver.findElement(MobileBy.AccessibilityId("2")));
+//        (new WebDriverWait(driver, 30)).until(ExpectedConditions.elementToBeClickable(buttonTwo));
 //        MobileElement buttonTwo = (MobileElement)(driver.findElement(MobileBy.AccessibilityId("2")));
 //        MobileElement buttonThree = (MobileElement)(driver.findElement(MobileBy.AccessibilityId("3")));
 //        MobileElement buttonPlus = (MobileElement)(driver.findElement(MobileBy.AccessibilityId("+")));
@@ -140,6 +168,7 @@ public class TestSetup {
 
       IOSDriver driver = createDriver(platformName, platformVersion, deviceName, method.getName());
 
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         /* Get the elements. */
         MobileElement buttonTwo = (MobileElement)(driver.findElement(MobileBy.AccessibilityId("2")));
         MobileElement buttonThree = (MobileElement)(driver.findElement(MobileBy.AccessibilityId("3")));
